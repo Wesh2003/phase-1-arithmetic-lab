169da26ee1b8a0d279c2a5e8b5e86a7693461454
Tumaini Muriithi <wendomuriithi@gmail.com>
2023-10-05 09:44:37 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1;
+let num2;
+let multiply = num1 * num262;
+num1 * num2 === 62;
+let random = Math.random();
+let num3;
+let num4;
+let mod = num3 %num4;
+num3 % num4 === 4;
+let max = Math.max(20);
+
+
